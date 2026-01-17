8e3b1b5909c3adf72ba55e667607baea5b15bbf3
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2026-01-17 00:01:52 -0800

    remove unused config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,5 @@
 import appiumConfig from '@appium/eslint-config-appium-ts';
 
-import typescript from '@typescript-eslint/eslint-plugin';
-import typescriptParser from '@typescript-eslint/parser';
-import prettier from 'eslint-plugin-prettier';
-
 export default [
   ...appiumConfig,
   {
@@ -25,40 +21,3 @@ export default [
     ignores: ['dist/', 'node_modules/', '*.js'],
   },
 ];
-
-  //   files: ['src/**/*.ts'],
-  //   languageOptions: {
-  //     parser: typescriptParser,
-  //     parserOptions: {
-  //       ecmaVersion: 'latest',
-  //       sourceType: 'module',
-  //     },
-  //     globals: {
-  //       console: 'readonly',
-  //       process: 'readonly',
-  //       Buffer: 'readonly',
-  //       __dirname: 'readonly',
-  //       __filename: 'readonly',
-  //       global: 'readonly',
-  //       module: 'readonly',
-  //       require: 'readonly',
-  //       exports: 'readonly',
-  //     },
-  //   },
-  //   plugins: {
-  //     '@typescript-eslint': typescript,
-  //     prettier: prettier,
-  //   },
-  //   rules: {
-  //     ...appiumConfig.rules,
-  //     // Keep project-specific overrides here; base rules come from appiumConfig
-  //     'no-console': 'off',
-  //     'prettier/prettier': 'error',
-  //     '@typescript-eslint/explicit-function-return-type': 'off',
-  //     '@typescript-eslint/explicit-module-boundary-types': 'off',
-  //     '@typescript-eslint/no-explicit-any': 'off',
-  //     '@typescript-eslint/ban-ts-comment': 'off',
-  //     'no-undef': 'off', // TypeScript handles this
-  //     'no-case-declarations': 'off',
-  //   },
-  // },
